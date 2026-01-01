eed25f0eb0c15046d81365dff7dcc219bbf23346
minhblackcool <minhblackcool@gmail.com>
2026-01-01 21:42:52 +0700

    Update Fibonacci.c

--- fibonacci/Fibonacci.c
+++ fibonacci/Fibonacci.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
- int main(){
- 	int n;
- 	printf("Nhap Gia Tri Cua n:");
- 	scanf("%d",&n);
- 	int f[n+1];
- 	f[0]=0;
- 	f[1]=1;
- 	for(int i=2;i<=n;i++){
- 		f[i]=f[i-1]+f[i-2];
-	 }
-	 printf("fibonaccy:");
-	 for(int i=0;i<=n;i++){
-	 printf("%d ",f[i]);}
-	 return 0;
- }
+
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    printf("Nhap gia tri cua n: ");
+    scanf("%d", &n);
+    printf("Day Fibonacci: ");
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    return 0;
+}
